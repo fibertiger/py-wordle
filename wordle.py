2d12428223b7bdc8769df0eb600b424c0cd0ec94
j <morcybollion@gmail.com>
2022-08-23 10:48:55 +0300

    win message fix

--- wordle.py
+++ wordle.py
@@ -1,5 +1,3 @@
-# to-do: real-life dictionary; checking if the input word is a real word
-
 import sys
 
 def compare(compare_with, compare_this):
@@ -7,7 +5,7 @@ def compare(compare_with, compare_this):
         if compare_this[i] == compare_with[i]: # if letter matched perfectly
             guessing_stat[i] = "+"
         else:
-            try: # if a letter from compared list is on a different spot
+            try: # trying if a letter is just in a different spot of the word
                 different_spot = compare_this.index(compare_with[i])
             except ValueError:
                 continue
@@ -21,22 +19,24 @@ secret_list = tuple(secret_word)
 print("Enter your guess:")
 
 guesses = 5
-while guesses > 0:
+while guesses > 0: # game process
     player_guess = input()
     if len(player_guess) == 5:
         if player_guess == secret_word:
+            guessing_stat = ["+", "+", "+", "+", "+"]
+            print(f"{guessing_stat[0]}{guessing_stat[1]}{guessing_stat[2]}{guessing_stat[3]}{guessing_stat[4]}") # ungracious way of printing the current guess' score to fit under the letters
             print("The word was: " + secret_word + ". You won!")
             sys.exit()
         else:
             guesses -= 1
 
-            guessing_stat = ["-", "-", "-", "-", "-"] # reverting this every time
+            guessing_stat = ["-", "-", "-", "-", "-"] # reverting the current guess' score to default before every next guess
 
             player_guess = list(player_guess) # https://stackoverflow.com/questions/113655/is-there-a-function-in-python-to-split-a-word-into-a-list
 
             compare(player_guess, secret_list)
 
-            print(f"{guessing_stat[0]}{guessing_stat[1]}{guessing_stat[2]}{guessing_stat[3]}{guessing_stat[4]}") # ungracious way of printing the output to fit under the letters
+            print(f"{guessing_stat[0]}{guessing_stat[1]}{guessing_stat[2]}{guessing_stat[3]}{guessing_stat[4]}") # ungracious way of printing the current guess' score to fit under the letters
             print(f"{guesses} tries left")
     else:
         print("The word should be 5 letters long!")
