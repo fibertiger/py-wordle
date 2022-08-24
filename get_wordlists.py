adc229fdd34b5493f303d550780b1ba2e15dc127
j <morcybollion@gmail.com>
2022-08-24 14:55:50 +0300

    switched to pickle module over ugly solutions

--- get_wordlists.py
+++ get_wordlists.py
@@ -1,38 +1,32 @@
 # transforming raw .txts into items of a list that we can use later
-
+import pickle as pkl
 
 oldanswers = open("rawwordlists/old_allowedanswers.txt","r") # allowed answers/"secret words"
-allowedanswers = open("allowedanswers.py","w")
-
 oldguesses = open("rawwordlists/old_allowedguesses.txt","r") # allowed guesses
-allowedguesses = open ("allowedguesses.py","w")
 
 
-def raw_to_list(oldfile, newfile, c, listname):
+def raw_to_list(oldfile, c):
+    new_list = []
     # crudely creating a list
-    def transform_words():
-        l = 6
-        for i in range(c):
-            word = oldfile.readline(l)
-            word = "  \"" + word
-            word = word[:6] + "\"" + ',\n'
-            newfile.write(word)
-            l = l + 5
-    
-    newfile.write(listname + " = [")
-    transform_words()
-    newfile.write("]")
-
+    l = 6
+    for i in range(c):
+        word = oldfile.readline(l)[:5]
+        new_list.append(word)
+        l = l + 5
+    return new_list
 
 def count_lines(file):
-    # find out many lines/words are in a file
-    # thanks to https://stackoverflow.com/questions/32607370/python-how-to-get-the-number-of-lines-in-a-text-file
+    # find out many lines/words are in a file; thanks to https://stackoverflow.com/questions/32607370/python-how-to-get-the-number-of-lines-in-a-text-file
     with open(file) as f: 
         for line in f:
             count = sum(1 for _ in f)
     return count
 
 
-raw_to_list(oldanswers, allowedanswers, count_lines("rawwordlists/old_allowedanswers.txt"), "a_list")
+a_list = raw_to_list(oldanswers, count_lines("rawwordlists/old_allowedanswers.txt"))
+a_file = open('allowed_answers.pkl', 'wb')
+pkl.dump(a_list, a_file)
 
-raw_to_list(oldguesses, allowedguesses, count_lines("rawwordlists/old_allowedguesses.txt"), "g_list")
+g_list = raw_to_list(oldguesses, count_lines("rawwordlists/old_allowedguesses.txt"))
+g_file = open('allowed_guesses.pkl', 'wb')
+pkl.dump(g_list, g_file)
